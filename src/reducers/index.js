231ee64c9d7a1a1e3be8c97376c15e876f733ec7
Scott Bowler <scott.bowler@learningfuze.com>
2018-01-24 12:20:21 -0800

    Completed getting movie quotes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import userReducer from './user_reducer';
+import movieReducer from './movie_reducer';
 
 export default combineReducers({
     user: userReducer,
-    form: formReducer
+    form: formReducer,
+    movie: movieReducer
 });
